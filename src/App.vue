aebaff718760eeda6992ba9cce4bea4b572e399b
Paul Bennett <pbennett.uk@outlook.com>
2020-08-17 19:22:18 +0100

    add upload working just need a working creative

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,33 @@
 <template>
-    <v-app>
-        <v-app-bar app color="primary" dark>
-            <h3>PulsePoint Generator</h3>
-            <v-spacer></v-spacer>
-            <v-btn-toggle v-model="display_size">
-                <v-btn>
-                    Mobile
-                </v-btn>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <h3>PulsePoint Generator</h3>
+      <v-spacer></v-spacer>
 
-                <v-btn>
-                    Desktop
-                </v-btn>
-            </v-btn-toggle>
-        </v-app-bar>
+      <v-btn-toggle v-model="display_size">
+        <v-btn>Mobile</v-btn>
 
-        <v-main>
-            <v-sitegen />
-        </v-main>
-    </v-app>
+        <v-btn>Desktop</v-btn>
+      </v-btn-toggle>
+    </v-app-bar>
+
+    <v-main>
+      <v-sitegen />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-    import SiteGenerator from './components/Sitegen';
-    export default {
-        name: 'App',
+  import SiteGenerator from "./components/Sitegen";
+  export default {
+    name: "App",
 
-        components: {
-            'v-sitegen': SiteGenerator,
-        },
+    components: {
+      "v-sitegen": SiteGenerator,
+    },
 
-        data: () => ({
-            display_size: null,
-        }),
-    };
+    data: () => ({
+      display_size: null,
+    }),
+  };
 </script>
--- src/components/Sitegen.vue
+++ src/components/Sitegen.vue
@@ -2,24 +2,24 @@
   <div class="mt-12 p-4 flex">
     <div class="w-2/5 px-24">
       <v-form ref="creative-upload">
-        <v-text-field :rules="nameRules" v-model="brand" label="Brand Name" required></v-text-field>
-        <v-text-field
-          :rules="nameRules"
-          v-model="url"
-          label="Site Url"
-          placeholder="Enter full url inc. https://"
-          required
-        ></v-text-field>
-        <!-- <v-text-field v-model="headline" :rules="nameRules" label="Headline" required></v-text-field> -->
-        <input type="file" round class="my-4" @change="onFileChange" />
-        <!-- <v-btn color="blue-grey" class="ma-2 white--text" @click="applyCreative">
-          <v-icon left dark>mdi-cloud-upload</v-icon>upload creative
-        </v-btn>-->
+        <div>
+          <h2 class="font-bold text-xl text-gray-500">Select a site to display</h2>
+          <v-overflow-btn class="my-2" :items="sites" v-model="url" label="Select Site"></v-overflow-btn>
+        </div>
+        <div class="my-8">
+          <h2 class="font-bold text-xl text-gray-500">Choose a creative as an image</h2>
+          <input type="file" round class="my-4" @change="onFileChange" />
+        </div>
+        <div class="my-8">
+          <h2 class="font-bold text-xl text-gray-500 mb-4">Upload a creative by a tag</h2>
+          <v-textarea solo name="input-7-4" label="Paste your script here" v-model="tag"></v-textarea>
+          <v-btn @click="uploadCreative">Upload Creative</v-btn>
+        </div>
       </v-form>
 
-      <div class="flex flex-col items-center justify-center border py-12">
+      <div class="flex flex-col items-center justify-center py-12">
         <div class="creative-outer break-words pt-1 pb-4">
-          <div>
+          <div id="creative-upload">
             <img :src="creative" />
           </div>
           <div class="text-left mt-2">
@@ -32,10 +32,13 @@
         </div>
       </div>
     </div>
+
     <div class="w-3/5">
       <iframe v-bind:src="url" width="100%" height="1000" id="iframe"></iframe>
     </div>
   </div>
+
+  <!-- <script src="https://tag.contextweb.com/TagPublish/getjs.aspx?action=VIEWAD&cwrun=200&cwadformat=728X90&cwpid=560884&cwwidth=728&cwheight=90&cwpnet=1&cwtagid=714180"></script> -->
 </template>
 
 <script>
@@ -45,8 +48,14 @@
         brand: "",
         headline: "",
         creative: "",
+        tag: "",
         url: "",
-        iframe: document.getElementById("iframe"),
+        sites: [
+          "https://www.independent.co.uk/",
+          "https://www.cbsnews.com/",
+          "https://uk.news.yahoo.com/",
+          "Verdana",
+        ],
       };
     },
     mounted: {
@@ -74,6 +83,14 @@
           vm.creative = e.target.result;
         };
         reader.readAsDataURL(file);
+      },
+      uploadCreative() {
+        const d = document.createElement("div");
+        const tag = this.tag;
+        d.innerHTML = tag;
+
+        const creative = document.getElementById("creative-upload");
+        creative.appendChild(d);
       },
     },
   };
